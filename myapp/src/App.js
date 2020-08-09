a04c30b9bc168a92d08b132f2b29c6406ff64fc3
Keshava Pranath K <kpranath@gmail.com>
2020-08-09 10:45:03 +0530

    nested components

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Ninjas from './Ninjas';
 
 function App() {
   return (
     <div className="App">
-
+      <h3>Welcome</h3>
+      <Ninjas />
     </div>
   );
 }
---
+++ myapp/src/Ninjas.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export class Ninjas extends Component {
+    render() {
+        return (
+            <div className='ninjas'>
+                <p>Name: Prnath</p>
+                <p>Age: 22</p>
+                <p>Belt: Black</p>
+            </div>
+        )
+    }
+}
+
+export default Ninjas;
