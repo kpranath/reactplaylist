1d2b44abdade265dc9da416117208589ab5e2fa7
Keshava Pranath K <kpranath@gmail.com>
2020-08-09 23:10:30 +0530

    forms

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Ninjas from './Ninjas';
+import AddNinjas from './AddNinja';
 
 class App extends Component {
   state = {
@@ -14,6 +15,7 @@ class App extends Component {
     return (
       <div className="App">
         <h3>Welcome</h3>
+        <AddNinjas />
         <Ninjas ninjas={this.state.ninjas} />
       </div>
     );
